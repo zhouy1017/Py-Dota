5b6553a6d3c24ee3a181d7aff93c2586c85b64b3
ChoW <chooow.yang@outlook.com>
2019-04-04 21:59:16 -0400

    base function done TODO: add specific url and date for each completed tournament

--- base_tools.py
+++ base_tools.py
@@ -5,7 +5,7 @@ class League:
     def __init__(self, name,links,date):
         self.name = name
         self.links = links
-        self.date = date
+        self.date = date # the date is the split date instead of starting date of the tournament
 
 def links_creater(link,pages):
     links = []
@@ -15,8 +15,26 @@ def links_creater(link,pages):
         links.append(link + "&page=" + str(i))
         i = i + 1
     return links
+
+def date_compare(date1,date2):
+    date1_list = date1.split("-")
+    date2_list = date2.split("-")
+    i = 0
+    while i < 3:
+        if date1_list[i] == date2_list[i]:
+            i = i + 1
+            continue
+        if date1_list[i] > date2_list[i]:
+            return 0
+        else:
+            return 1
+        
+    raise Exception('the split date should not equal to any actual match date.')
+
 def get_matchid_of_league(league):
     match_table = {}
+    qualifier = []
+    main_event = []
     for link in league.links:
         target = link
         # fake UA to bypass Dotabuff's anti spider mechanism
@@ -37,18 +55,27 @@ def get_matchid_of_league(league):
             date_list = bf_sub.time['datetime'].split('T')
             date = date_list[0]
             match_table[match_id] = date
+            if date_compare(date,league.date) == 1:
+                qualifier.append(match_id)
+            else:
+                main_event.append(match_id)
 
         time.sleep(1) # limit request rate to prevent unusual traffic toward Dotabuff
         # If code runs normally, we should only request each page once 
         
 
 
-    return match_table
+    return match_table, qualifier, main_event
 
-def differ_qua(matchid_list, split_time):
-    return
 
-links_b = links_creater("https://www.dotabuff.com/esports/leagues/10452-the-bucharest-minor-2019/matches?original_slug=10452-the-bucharest-minor-2019",7)
-B_Minor = League('Bucharest Minor',links_b,"2019-01-08")
-table = get_matchid_of_league(B_Minor)
-print(len(table))
+
+def differed_id(league_name):
+    league = None
+    if league_name == "Bucharest Minor":
+        links_b = links_creater("https://www.dotabuff.com/esports/leagues/10452-the-bucharest-minor-2019/matches?original_slug=10452-the-bucharest-minor-2019",7)
+        league = League('Bucharest Minor',links_b,"2019-01-08")
+    
+    table,qualifier,main_event = get_matchid_of_league(league)
+    return table, qualifier, main_event
+    
+
