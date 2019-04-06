d60979dfea1ee34e847d943bc8cedd0940e6f855
ChoW <chooow.yang@outlook.com>
2019-04-06 10:51:00 -0400

    Draft Done for all basic functions.

---
+++ opendota_api.py
@@ -0,0 +1,6 @@
+import requests
+import pandas as pd
+import base_tools as tools
+
+# List of columns 
+# Player_id Player_nickname Kills Death Assists Roshan_kill Tower_kill Participation GPM CS FB Stuns Runes Wards Camps
